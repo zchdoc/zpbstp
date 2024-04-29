c33161f6724c8a8a8e9177a61f4214e2ceab6613
zchcpyw10 <zghnzch@163.com>
2024-04-29 10:05:32 +0800

    Create push_git.bat

---
+++ push_git.bat
@@ -0,0 +1,11 @@
+@REM 不需要这两行的可以去掉
+set http_proxy=http://127.0.0.1:10809
+set https_proxy=http://127.0.0.1:10809
+
+git push origin_gitee
+echo "push origin_gitee success"
+git push origin_github
+echo "push origin_github success"
+git push origin_gitlab
+echo "push origin_gitlab success"
+
