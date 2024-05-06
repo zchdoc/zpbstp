b825927179c12142516f75464b912ff219077735
zchcpyw10 <zghnzch@163.com>
2024-05-06 09:17:39 +0800

    add some url 20240506

--- script/work-git/pull_git.bat
+++ script/work-git/pull_git.bat
@@ -2,6 +2,8 @@
 set http_proxy=http://127.0.0.1:10809
 set https_proxy=http://127.0.0.1:10809
 
+cd ../../
+
 git pull origin_gitee Dev22-0617-DeployServer
 echo "pull origin_gitee success"
 git pull origin_github  Dev22-0617-DeployServer
--- script/work-git/push_git.bat
+++ script/work-git/push_git.bat
@@ -2,6 +2,8 @@
 set http_proxy=http://127.0.0.1:10809
 set https_proxy=http://127.0.0.1:10809
 
+cd ../../
+
 git push origin_gitee
 echo "push origin_gitee success"
 git push origin_github
