f6c454fa46d17487f8dff88ef22b36e6edc47776
ZchMac2020M1 <zghnzch@163.com>
2024-06-19 19:27:33 +0800

    整理脚本

--- script/work-git/push_git.sh
+++ script/work-git/push_git.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# 不需要这两行的可以去掉
-#export https_proxy=http://127.0.0.1:10809
-#export http_proxy=http://127.0.0.1:10809
-#export all_proxy=socks5://127.0.0.1:10809
-
+# comment out the following code if you don't need it
+export https_proxy=http://127.0.0.1:10809 http_proxy=http://127.0.0.1:10809 all_proxy=socks5://127.0.0.1:10809
 cd ../../
-
+# comment out the code below if you do not need it
 git push origin_gitee
 echo "push origin_gitee success"
 git push origin_github
